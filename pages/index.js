3af76d5353ddd993260a91f10d4f5ee4df310c6b
Raihanul-Islam-Rahat <md.raihanulislam.x@gmail.com>
2025-07-07 02:16:53 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function KairosDashboard() {
